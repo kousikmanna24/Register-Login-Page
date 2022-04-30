13ea3c7175700fbdc836ec03752f307bb4842b60
Kousik manna <93304061+kousikmanna24@users.noreply.github.com>
2022-04-30 22:58:32 +0530

    Add files via upload

---
+++ website.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>first page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Technical Suggest</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" href="website.php"> HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">REGISTER</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">login</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+
+<style >
+   .jumbotron {
+    background-color: skyblue;
+  }
+  .jumbotron p {
+    font-size: 20px;
+  }
+
+</style>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>My First Register And Login Project</h1>
+  <p>Do The Register And Login for check its work properly or not ?</p> 
+</div>
+
+<style >
+   .image {
+    height: 100%;
+    width: 100%;
+    background-color: green;
+   }
+  
+</style>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fakeimg"><img class="image" src="image/WSB1.jpg"></div>
+      <p>I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience....  :) </p>
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg"><img class="image" src="image/sign.jpg"></div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....   :)</p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>copyright@Kousikproduction@2022</p>
+</div>
+
+</body>
+</html>
